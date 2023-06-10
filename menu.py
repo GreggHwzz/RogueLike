50ef4bcfccb066fa5974aad1c9fc164f3ec8872e
GreggHwzz <gtallaapro@gmail.com>
2023-06-10 15:39:18 +0200

    Ajout de la musique

--- menu.py
+++ menu.py
@@ -5,16 +5,8 @@ from character import Character
 
 pygame.init()
 
-ecran_info = pygame.display.Info()
-largeur = ecran_info.current_w
-hauteur = ecran_info.current_h
-
-SCREEN = pygame.display.set_mode((largeur, hauteur), pygame.FULLSCREEN)
+SCREEN = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 pygame.display.set_caption("Menu")
-
-MUSIC = pygame.mixer.music.load('music/main_theme.mp3')
-pygame.mixer.music.set_volume(0.08)
-pygame.mixer.music.play(-1)
 
 clock = pygame.time.Clock()
 
@@ -23,7 +15,6 @@ def get_font(size): # Returns Press-Start-2P in the desired size
 
 def play():
     clock = pygame.time.Clock()
-    
     #Definition des variables de mouvements
     moving_left = False
     moving_right = False
@@ -32,17 +23,10 @@ def play():
 
     #Création de personnage
     player = Character(100,100)
-    
     while True:
         clock.tick(constants.FPS)
     
         SCREEN.fill(constants.BG)
-        
-        EXIT_BUTTON = Button(image=None, pos=(100,40), text_input="Quitter", font=get_font(20), base_color="Grey", hovering_color="White")
-        BACK_BUTTON = Button(image=None, pos=(300,40), text_input="Retour", font=get_font(20), base_color="Grey", hovering_color="White")
-        
-        MENU_MOUSE_POS = pygame.mouse.get_pos()
-        
         #Calcul des mouvements du joueur
         dx = 0
         dy = 0
@@ -62,22 +46,9 @@ def play():
         #Affichage du personnage sur l'écran
         player.draw(SCREEN)
     
-        for button in [EXIT_BUTTON, BACK_BUTTON]:
-            button.changeColor(MENU_MOUSE_POS)
-            button.update(SCREEN)
-        
         for event in pygame.event.get():
-            
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-                
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if EXIT_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    pygame.quit()
-                    sys.exit()
-                if BACK_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    main_menu()
+                run = False
             
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_z:
@@ -105,24 +76,17 @@ def options():
     while True:
         OPTIONS_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN.fill("Black")
+        SCREEN.fill("white")
 
-        OPTIONS_TEXT = get_font(100).render("OPTIONS", True, "White")
-        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(SCREEN.get_width()//2, 190))
+        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        OPTIONS_BACK = Button(image=None, pos=(SCREEN.get_width()//2, 700), 
-                            text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
-        
-        MUSIC_OFF = Button(image=None, pos=(600,460), 
-                            text_input="MUSIC OFF", font=get_font(75), base_color="Brown", hovering_color="RED")
-        
-        MUSIC_ON = Button(image=None, pos=(SCREEN.get_width()-600, 460), 
-                            text_input="MUSIC ON", font=get_font(75), base_color="#007113", hovering_color="#00D123")
-        
-        for button in [OPTIONS_BACK,MUSIC_ON,MUSIC_OFF]:
-            button.changeColor(OPTIONS_MOUSE_POS)
-            button.update(SCREEN)
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), 
+                            text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+
+        OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
+        OPTIONS_BACK.update(SCREEN)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -131,20 +95,15 @@ def options():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if OPTIONS_BACK.checkForInput(OPTIONS_MOUSE_POS):
                     main_menu()
-                if MUSIC_OFF.checkForInput(OPTIONS_MOUSE_POS):
-                    pygame.mixer.music.pause()
-                if MUSIC_ON.checkForInput(OPTIONS_MOUSE_POS):
-                    pygame.mixer.music.unpause()
-                        
+
         pygame.display.update()
-        
-    
+
 def main_menu():
     while True:
         SCREEN.fill((1,0,0))
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
-        MENU_TEXT = get_font(100).render("Efreyan Adventures", True, "#057DC1")
+        MENU_TEXT = get_font(100).render("Efreyan Adventures", True, "#b68f40")
         MENU_RECT = MENU_TEXT.get_rect(center=(SCREEN.get_width()//2,200))
 
         PLAY_BUTTON = Button(image=pygame.image.load("assets/Play Rect.png"), pos=(SCREEN.get_width()//2, ((SCREEN.get_height()//2)-100)), 
