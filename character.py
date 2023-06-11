1861d7cf0cbdad4c0d4c0d240eb10a40486d489e
GreggHwzz <gtallaapro@gmail.com>
2023-06-11 12:48:02 +0200

    Correction de bug

--- character.py
+++ character.py
@@ -4,7 +4,6 @@ import constants
 
 class Character():
     def __init__(self,x,y):
-<<<<<<< HEAD
          self.rect = pygame.Rect(0,0,70,70)
          self.rect.center = (x,y)
          self.image = pygame.image.load("assets/doux.png").convert_alpha()
@@ -12,21 +11,14 @@ class Character():
     def draw(self,surface, frame):
         surface.blit(self.image, self.rect, pygame.Rect(48*frame, 0, 48, 48))
         self.image.set_colorkey((0,0,0))
-=======
         self.rect = pygame.Rect(0, 0, 48, 48)
         self.rect.center=(x,y)
         self.image = pygame.image.load("doux.png").convert_alpha()
          
-
-         
     def draw(self,surface,frame):
          surface.blit(self.image, self.rect, pygame.Rect(48*frame, 0, 48, 48))
          self.image.set_colorkey((0,0,0))
-
->>>>>>> 3ff03e4022ce7f698dde009cde4a25d6f5982d36
     
-
-
     def move(self,dx,dy):
         
         if dx != 0 and dy != 0:
